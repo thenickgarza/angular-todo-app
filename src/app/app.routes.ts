0e6eb767a0bcc2120083cffe328eef02549f9c29
Nick Garza <thenickgarza14@gmail.com>
2025-11-24 16:04:09 -0600

    start of weather dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { TodoList } from './todo/todo-list/todo-list.component';
+import { WeatherDashboard } from './weather-dashboard/weather-dashboard/weather-dashboard.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: TodoList
+    },
+    {
+        path: 'weather',
+        component: WeatherDashboard
     }
 ];
