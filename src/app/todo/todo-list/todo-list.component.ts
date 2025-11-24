d7bc360af3ad183359ffa4eb521635a63002645c
Nick Garza <thenickgarza14@gmail.com>
2025-11-24 14:17:52 -0600

    added addTodo to the todo list component. In the HTML we are also setting up the input values and then calling addTodo and supplying the values from the input.

--- src/app/todo/todo-list/todo-list.component.ts
+++ src/app/todo/todo-list/todo-list.component.ts
@@ -7,19 +7,31 @@ import { Todo } from '../todo.model';
   templateUrl: './todo-list.html',
   styleUrl: './todo-list.css',
 })
-
-
-
 export class TodoList {
-  
   todos = signal<Todo[]>([
     {
       id: '1',
       title: 'Buy groceries',
       description: 'Buy groceries for the week',
       completed: false,
-      createdAt: new Date()
-    }
+      createdAt: new Date(),
+    },
   ]);
-  
+
+  addTodo(title: string, description: string) {
+   // Since this is a signal, we need to update the signal using update method. 
+    this.todos.update((todos) => {
+      return [
+        // ...todos is the spread operator to add the existing todos to the new array. 
+        ...todos,
+        {
+          id: `${todos.length + 1}`,
+          title: title,
+          description: description,
+          completed: false,
+          createdAt: new Date(),
+        },
+      ];
+    });
+  }
 }
