6a871b393cfde282cc5e2b1954b151ebd11fdb59
Nick Garza <thenickgarza14@gmail.com>
2025-11-25 10:31:16 -0600

    current weather forecast returning. Need to use switchmap when chaining API requests together. Subscribe in the component.

--- src/app/weather-dashboard/weather-dashboard/weather-dashboard.component.ts
+++ src/app/weather-dashboard/weather-dashboard/weather-dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { WeatherService } from './weather-service.service';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-weather-dashboard',
@@ -7,15 +8,20 @@ import { WeatherService } from './weather-service.service';
   templateUrl: './weather-dashboard.html',
   styleUrl: './weather-dashboard.css',
 })
-
-
 export class WeatherDashboard {
+  constructor(private weatherService: WeatherService) {}
 
-  constructor(private weatherService: WeatherService) { }
-
-  getGeoLocation(city:string) {
-    this.weatherService.getGeoLocation(city).subscribe((data) => {
+  getWeather(city: string) {
+    this.getGeoLocation(city).subscribe((data) => {
       console.log(data);
     });
+  }
+
+  getGeoLocation(city: string) {
+    return this.weatherService.getGeoLocation(city).pipe(
+      switchMap((data: any[]) => {
+        return this.weatherService.getWeather(data[0].lat, data[0].lon);
+      })
+    );
   }
 }
--- src/app/weather-dashboard/weather-dashboard/weather-service.service.ts
+++ src/app/weather-dashboard/weather-dashboard/weather-service.service.ts
@@ -5,18 +5,24 @@ import apiKeys from '../../../api-keys.json';
 @Injectable({
   providedIn: 'root',
 })
+
 export class WeatherService {
 
   private apiKey = apiKeys.API_KEY;
   
   private apiGeoUrl = "http://api.openweathermap.org/geo/1.0/direct"
+  private apiWeatherUrl = "https://api.openweathermap.org/data/2.5/weather"
 
   constructor(private http: HttpClient) { }
   
   getGeoLocation(city:string) {
-    
     const url = `${this.apiGeoUrl}?q=${city}&limit=5&appid=${this.apiKey}`
-    return this.http.get(url);
+    return this.http.get<any[]>(url);
+  }
+
+  getWeather(lat:string, lon:string) {
+    const url = `${this.apiWeatherUrl}?lat=${lat}&lon=${lon}&appid=${this.apiKey}`
+    return this.http.get<any>(url);
   }
 
 }
