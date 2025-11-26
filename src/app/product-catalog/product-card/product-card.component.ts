1c04008f6476a50be6166bd1a586164db3a6b3a8
Nick Garza <thenickgarza14@gmail.com>
2025-11-26 17:12:33 -0600

    set up links for each card to link to /products/:id

--- src/app/product-catalog/product-card/product-card.component.ts
+++ src/app/product-catalog/product-card/product-card.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Product } from '../product.model';
+import { RouterLink } from '@angular/router';
 
 
 @Component({
   selector: 'app-product-card',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './product-card.html',
   styleUrl: './product-card.css',
 })
