047cf902377623dd250c8eada180cfde2ca812f7
Nick Garza <thenickgarza14@gmail.com>
2025-12-01 09:39:22 -0600

    starting the cart, injected the product service into shopping cart.

--- src/app/product-catalog/shopping-cart/shopping-cart.ts
+++ src/app/product-catalog/shopping-cart/shopping-cart.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { ProductService } from '../product-service';
+import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
+import { CartItem } from '../product.model';
 
 @Component({
   selector: 'app-shopping-cart',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './shopping-cart.html',
   styleUrl: './shopping-cart.css',
 })
 export class ShoppingCart {
+
+  cartItems$: Observable<CartItem[]>;
+
+  constructor(private productService: ProductService) {
+    // get the observabkle
+    this.cartItems$ = this.productService.getCartItems();
+  }
+
+  removeItem(id: number) {
+   this.productService.removeFromCart(id)
+  }
+
+  getTotal(): number {
+    return this.productService.getCartTotal();
+  }
+
+  
+
+  
 
 }
